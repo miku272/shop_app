13c77692c63f743ce01ccc90034c09d8644a6c50
Naresh Sharma <sharmanaresh272@gmail.com>
2022-06-05 21:02:28 +0530

    Revoked all secrets

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -3,6 +3,8 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
+import '../confidential/api_keys.dart';
+
 class Auth with ChangeNotifier {
   String _token;
   DateTime _expiryDate;
@@ -16,7 +18,7 @@ class Auth with ChangeNotifier {
 
   Future<void> signUp(String email, String password) async {
     final url = Uri.parse(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBGmoEF6GBNd6ixqa1qSZPvyvyFOxVHD6A');
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=$firebaseApi');
 
     final response = await http.post(
       url,
